eb12e5d8d1e590d7a55f04bfedba737f379a2051
Jabrail Lezgintsev <lezgintsev13@yandex.ru>
2018-02-17 22:15:43 +0300

    Added Hard-coding avoiding

--- app/tests.py
+++ app/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.test import Client
 from django.contrib.auth.models import User
+from django.shortcuts import reverse
 
 
 class URLsTesting(TestCase):
@@ -12,19 +13,25 @@ class URLsTesting(TestCase):
 
     def test_urls(self):
 
-        urls = ('/',
-                '/admin/',
-                '/add/',
-                '/channel/new/',
-                '/channels',
-                '/accounts/login/',
-                '/accounts/logout/',
-                '/login/facebook/',
-                '/login/vk-oauth2/',
-                '/i18n/setlang/',
+        urls = ('index',
+                'create-playlist',
+                'new-channel',
+                'channels',
+                'login',
+                'logout'
                 )
 
+        hard_urls = ('/login/facebook/',
+                     '/login/vk-oauth2/',
+                     '/i18n/setlang/'
+                     )
+
         for url in urls:
+            response = self.donald_trump.get(reverse(url), folow=True)
+            self.assertIn(response.status_code, self.ok, msg='Path: %s, Status code: %s,' %
+                                                             (response.request['PATH_INFO'], response.status_code))
+
+        for url in hard_urls:
             response = self.donald_trump.get(url, folow=True)
             self.assertIn(response.status_code, self.ok, msg='Path: %s, Status code: %s,' %
                                                              (response.request['PATH_INFO'], response.status_code))
