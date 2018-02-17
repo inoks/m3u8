f4b84cb3aee3fa2fa1e0fd476837a778fe86eb2a
sergey <inoks@mail.ru>
2018-02-17 23:09:10 +0700

    Removed unused import

--- playlist/settings.py
+++ playlist/settings.py
@@ -12,8 +12,6 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 
-from django.utils.translation import gettext_lazy as _
-
 ADMINS = []
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
