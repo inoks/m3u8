7ca1829739b7721749b2c546a9202b3181cd6333
Jabrail Lezgintsev <lezgintsev13@yandex.ru>
2018-02-17 21:06:02 +0300

    Added URLs testing

--- app/tests.py
+++ app/tests.py
@@ -1,6 +1,25 @@
 from django.test import TestCase
+from django.test import Client
 
 
 class AppTestCase(TestCase):
     def test_true(self):
         self.assertTrue(True)
+
+
+class URLsTesting(TestCase):
+
+    def setUp(self):
+        self.donald_trump = Client(enforce_csrf_checks=True)
+        self.ok = (200, 302)
+
+    def test_urls(self):
+
+        urls = ('/', '/admin/', '/add/', '/channel/new/',
+                '/channels', '/accounts/login/',
+                '/accounts/logout/', )
+
+        for url in urls:
+            response = self.donald_trump.get(url)
+            self.assertIn(response.status_code, self.ok, msg='Path: %s, Status code: %s,' %
+                                                             (response.request['PATH_INFO'], response.status_code))
