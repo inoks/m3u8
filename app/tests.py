ccce2d0ab8e6b7ade81fd2579b91b94e44ee9279
Jabrail Lezgintsev <lezgintsev13@yandex.ru>
2018-02-17 22:04:40 +0300

    Optional refactoring

--- app/tests.py
+++ app/tests.py
@@ -7,8 +7,7 @@ class URLsTesting(TestCase):
 
     def setUp(self):
         self.donald_trump = Client(enforce_csrf_checks=True)
-        user = User.objects.create_superuser('Garry Kasparov', 'garry1@ronda.com', 'garry500')
-        self.donald_trump.force_login(user)
+        self.donald_trump.force_login(User.objects.create_superuser('Garry Kasparov', 'garry1@ronda.com', 'garry500'))
         self.ok = (200, 302)
 
     def test_urls(self):
@@ -23,10 +22,9 @@ class URLsTesting(TestCase):
                 '/login/facebook/',
                 '/login/vk-oauth2/',
                 '/i18n/setlang/',
-                '',
                 )
 
         for url in urls:
-            response = self.donald_trump.get(url)
+            response = self.donald_trump.get(url, folow=True)
             self.assertIn(response.status_code, self.ok, msg='Path: %s, Status code: %s,' %
                                                              (response.request['PATH_INFO'], response.status_code))
