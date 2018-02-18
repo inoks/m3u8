825f52907e98bb3f455b74e9f9875a52667710ee
sergey <inoks@mail.ru>
2018-02-19 00:03:11 +0700

    Simplified Explicit is better than implicit. Don't use real name/email in tests or apps.

--- app/tests.py
+++ app/tests.py
@@ -1,67 +1,70 @@
-from django.test import TestCase
-from django.test import Client
 from django.contrib.auth.models import User
 from django.shortcuts import reverse
-from app.models import Channel, Playlist, Upload
-from django.utils import timezone
-from django.core.files.uploadedfile import SimpleUploadedFile
+from django.test import Client
+from django.test import TestCase
+
+from app.models import Channel, Playlist
 
 
-class URLsTesting(TestCase):
-
-    def setUp(self):
-        self.donald_trump = Client(enforce_csrf_checks=True)
-        self.donald_trump.force_login(User.objects.create_user('Garry Kasparov', 'garry1@ronda.com', 'garry500'))
-        self.ok = (200, 302)
-
-    def test_urls(self):
-
-        urls = ('index',
-                'create-playlist',
-                'new-channel',
-                'channels',
-                'login',
-                'logout'
-                )
-
-        hard_urls = ('/login/facebook/',
-                     '/login/vk-oauth2/',
-                     '/i18n/setlang/'
-                     )
-
-        for url in urls:
-            response = self.donald_trump.get(reverse(url), folow=True)
-            self.assertIn(response.status_code, self.ok, msg='Path: %s, Status code: %s,' %
-                                                             (response.request['PATH_INFO'], response.status_code))
-
-        for url in hard_urls:
-            response = self.donald_trump.get(url, folow=True)
-            self.assertIn(response.status_code, self.ok, msg='Path: %s, Status code: %s,' %
-                                                             (response.request['PATH_INFO'], response.status_code))
-
-
-class ModelsTesting(TestCase):
+class AppTestCase(TestCase):
 
     def setUp(self):
-        jeryy_user = User.objects.create_user('Jerry CZ', 'jerry@a1.com', '436zfer')
+        self.client = Client()
+        self.username = 'John Doe'
+        self.email = 'john@example.com'
+        self.password = 'dolphins'
+        self.user = User.objects.create_user(
+            username=self.username,
+            email=self.email,
+            password=self.password
+        )
+        self.client.login(
+            username=self.username,
+            password=self.password
+        )
 
-        self.playlist = Playlist(user=jeryy_user, created_at=timezone.datetime.today())
+        self.playlist = Playlist(user=self.user)
         self.playlist.save()
 
-        self.channel = Channel(playlist=self.playlist, title='Testing Playlist',
-                               duration='150 min', group='The best group', path='no path')
-        self.channel.save()
+        self.channel = Channel.objects.create(
+            playlist=self.playlist,
+            title='Testing Playlist',
+            duration='150',
+            group='The best group',
+            path='no path'
+        )
 
-        self.upload = Upload(user=jeryy_user, info='Hello! ', created_at=timezone.datetime.today(),
-                             file=SimpleUploadedFile('file_t.jpg', b'Nothing'))
-        self.upload.save()
+    def test_urls(self):
+        urls = [
+            'index',
+            'create-playlist',
+            'new-channel',
+            'channels',
+            'login',
+            'logout'
+        ]
+
+        social_auth_redirect_urls = [
+            '/login/facebook/',
+            '/login/vk-oauth2/',
+        ]
+
+        for url in urls:
+            response = self.client.get(reverse(url), follow=True)
+            self.assertEqual(response.status_code, 200, msg='Unable to open: %s' % url)
+
+        for url in social_auth_redirect_urls:
+            response = self.client.get(url)
+            self.assertEqual(response.status_code, 302, msg='Unable to open: %s' % url)
 
     def test_playlist(self):
-        self.assertTrue(self.playlist.public_link)
+        self.assertGreater(self.playlist.count, 0)
 
-        # self.assertTrue(self.playlist.get_absolute_url())  # Raises an error
+    def test_playlist_public_link(self):
+        self.assertIsNotNone(self.playlist.public_link)
 
-        self.assertTrue(self.playlist.count)
+        response = self.client.get(self.playlist.public_link)
+        self.assertEqual(response.status_code, 200)
 
-    def test_channel(self):
-        self.assertTrue(self.channel.get_absolute_url())
+    def test_channel_link(self):
+        self.assertIsNotNone(self.channel.get_absolute_url())
