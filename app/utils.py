4a83ca2ce67519c6f5b18ab6e0891640e2b1e392
Jabrail Lezgintsev <lezgintsev13@yandex.ru>
2018-03-02 13:40:29 +0300

    Modified, there were deleted several minor lines

--- app/utils.py
+++ app/utils.py
@@ -2,7 +2,7 @@ import logging
 import random
 import string
 
-
+import m3u8
 
 import requests
 from django.core.files.base import ContentFile
@@ -13,44 +13,6 @@ logger = logging.getLogger(__name__)
 def generate_random_key(length=4):
     return ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(length))
 
-
-def extinf_format(fo, playlist):
-    from app.models import Channel
-    # import re
-    import m3u8
-
-    fo.file.seek(0)
-    content = fo.read().decode('utf-8')
-
-    all = m3u8.loads(content)
-
-    for segment in all.segments:
-
-        Channel.objects.create(
-            playlist=playlist,
-            title=segment.title if segment.title else None,
-            duration=segment.duration if segment.duration else None,
-            group=segment.group if hasattr(segment, 'group') else None,
-            path=segment.uri
-        )
-
-    # duration = re.search(r'EXTINF:(?P<duration>-?\d+)', content)
-    #
-    # group_title = re.search(r'group-title=(?P<group_title>".+")', content)
-    #
-    # http = re.search(r'(?P<group_title>((http|https):\/\/).+$)', content)
-    #
-    # title = re.search(r',(?P<group_title>.*) ', content)
-
-    # tvg_id = re.search(rb'tvg-id=(?P<tvg_id>"[^`"]+")', content)
-
-    # tvg_name = re.search(rb'tvg-name=(?P<tvg_name>".+")', content)
-
-    # tvg_logo = re.search(rb'tvg-logo=(?P<tvg_logo>"[^`"]+")', content)
-
-    # tvg_country = re.search(rb'tvg-country=(?P<group_title>"[^`"]+")', content)
-
-    # tvg_language = re.search(rb'tvg-language=(?P<group_title>"[^`"]+")', content)
 
 def load_remote_m3u8(link, playlist, remove_existed=False):
     from app.models import Channel, Upload
@@ -69,41 +31,21 @@ def load_remote_m3u8(link, playlist, remove_existed=False):
     if remove_existed:
         playlist.channels.all().delete()
 
-    duration = title = group = path = None
-    for line in r.iter_lines(decode_unicode=True):
-        if isinstance(line, bytes):
-            line = line.decode("utf-8")
+    channels = m3u8.load(link)
 
-        if line == '#EXTM3U':
-            continue
-
-        if line.startswith('#EXTINF:'):
-            duration, title = line[8:].split(',')
-            continue
-
-        if line.startswith('#EXTGRP:'):
-            group = line[8:]
-            continue
-
-        if line.startswith('#'):
-            logger.warning('Unsupported line skipped: {}'.format(line))
-            continue
-
-        if line:
-            path = line
-
+    if channels.segments:
+        for segment in channels.segments:
             Channel.objects.create(
                 playlist=playlist,
-                title=title,
-                duration=duration,
-                group=group,
-                path=path
+                title=segment.title if segment.title else None,
+                duration=segment.duration if segment.duration else None,
+                group=segment.group if hasattr(segment, 'group') else None,
+                path=segment.uri
             )
 
 
 def load_m3u8_from_file(fo, playlist, remove_existed=False):
     from app.models import Channel, Upload
-    import m3u8
 
     Upload.objects.create(
         user=playlist.user,
@@ -118,12 +60,10 @@ def load_m3u8_from_file(fo, playlist, remove_existed=False):
     if remove_existed:
         playlist.channels.all().delete()
 
-    duration = title = group = None
+    channels = m3u8.loads(content)
 
-    all = m3u8.loads(content)
-
-    if all.segments:
-        for segment in all.segments:
+    if channels.segments:
+        for segment in channels.segments:
             Channel.objects.create(
                 playlist=playlist,
                 title=segment.title if segment.title else None,
@@ -131,32 +71,3 @@ def load_m3u8_from_file(fo, playlist, remove_existed=False):
                 group=segment.group if hasattr(segment, 'group') else None,
                 path=segment.uri
             )
-
-    # for line in fo.read().splitlines():
-    #     line = line.decode("utf-8")
-    #
-    #     if line == '#EXTM3U':
-    #         continue
-    #
-    #     if line.startswith('#EXTINF:'):
-    #         duration, title = line[8:].split(',')
-    #         continue
-    #
-    #     if line.startswith('#EXTGRP:'):
-    #         group = line[8:]
-    #         continue
-    #
-    #     if line.startswith('#'):
-    #         logger.warning('Unsupported line skipped: {}'.format(line))
-    #         continue
-    #
-    #     if line:
-    #         path = line
-    #
-    #         Channel.objects.create(
-    #             playlist=playlist,
-    #             title=title,
-    #             duration=duration,
-    #             group=group,
-    #             path=path
-    #         )
