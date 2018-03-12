8e5d5a3008e23736f745827dfa84c43993d7eacd
sergey <inoks@mail.ru>
2018-03-12 15:58:30 +0200

    Updated styles, fixed multiple playlist issue, increased page size to 50

--- app/templatetags/extra_tags.py
+++ app/templatetags/extra_tags.py
@@ -5,9 +5,9 @@ register = template.Library()
 
 @register.simple_tag
 def url_replace(request, field, value):
-    """ To avoid get params lossing """
-    query_string = request.GET.copy()
+    """ To avoid GET params loosing """
 
+    query_string = request.GET.copy()
     query_string[field] = value
 
     return query_string.urlencode()
